32f5638fd5eb66d092777f1fabdd79241059fac6
junicotandiago198 <junicotandiagoo21@gmail.com>
2022-12-22 16:28:21 +0700

    create seeder & factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // FACTORY USER
         // \App\Models\User::factory(10)->create();
+
+        // FACTORY COMPANY
+        // \App\Models\Company::factory(10)->create();
+
+        // FACTORY TEAM
+        // \App\Models\Team::factory(10)->create();
+
+        // FACTORY ROLE
+        // \App\Models\Role::factory(10)->create();
+
+        // FACTORY RESPONSIBILITY
+        // \App\Models\Responsibility::factory(10)->create();
+
+        // FACTORY EMPLOYEE
+        \App\Models\Employee::factory(100)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
