b5b79936c4a5af950a43403e6cde1940bf9e028b
junicotandiago198 <junicotandiagoo21@gmail.com>
2022-12-22 10:23:22 +0700

    create relationship models

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Company extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'logo',
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function teams()
+    {
+        return $this->hasMany(Team::class);
+    }
+
+    public function roles()
+    {
+        return $this->hasMany(Roles::class);
+    }
 }
